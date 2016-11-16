34d21096aedd682a372bee5aad88ea02fac46980
kcho <sky8671@gmail.com>
2016-11-16 18:32:12 +0900

    medication effect graph logic added

--- extract_mean_values_ROI.m
+++ extract_mean_values_ROI.m
@@ -1,45 +1,59 @@
 function extract_mean_values_ROI(xSPM)
 ROI = xSPM.XYZ;
 
-spmLoc = sprintf('%s/SPM.mat', xSPM.swd)
-SPM = load(spmLoc)
-SPM = SPM.SPM
+spmLoc = sprintf('%s/SPM.mat', xSPM.swd);
+SPM = load(spmLoc);
+SPM = SPM.SPM;
 
-SPM
 Obese = {'HMS', 'NSW', 'HJC', 'JHS', 'CSJ', 'CDJ', 'KTH', 'SHJ', 'KDS', 'LJS', 'KHS', 'KYH', 'KYC', 'JJR'};  %KJS removed;
 Lean = {'JTS', 'JJY', 'MAS', 'LKB', 'KJW', 'KOS', 'KTO', 'KSO', 'YSS', 'CDK', 'YOI', 'SJS', 'PJO', 'CDH', 'LKH'};
+
+GLP = {'HMS_G','NSW_G','HJC_G','JHS_G','CSJ_G','CDJ_G','KTH_G','SHJ_G','KDS_G','LJS_G','KHS_G','KYH_G','KYC_G','JJR_G','JTS_G','JJY_G','MAS_G','LKB_G','KJW_G','KOS_G','KTO_G','KSO_G','YSS_G','CDK_G','YOI_G','SJS_G','PJO_G','CDH_G','LKH_G'};
+saline = {'HMS_S','NSW_S','HJC_S','JHS_S','CSJ_S','CDJ_S','KTH_S','SHJ_S','KDS_S','LJS_S','KHS_S','KYH_S','KYC_S','JJR_S','JTS_S','JJY_S','MAS_S','LKB_S','KJW_S','KOS_S','KTO_S','KSO_S','YSS_S','CDK_S','YOI_S','SJS_S','PJO_S','CDH_S','LKH_S'};
 
 averages = mean(spm_get_data(SPM.xY.P, ROI), 2);
 
 obese_means = [];
 lean_means = [];
+GLP_means = [];
+saline_means = [];
 
 for i=1:length(SPM.xY.P)
     subjectFileLoc = SPM.xY.P{i};
     startIndex = regexp(subjectFileLoc, '\/[A-Z]{3}_[A-Z]{1}\/');
-    subjectName = SPM.xY.P{i}(startIndex+1:startIndex+3);
-    if any(strcmp(Obese,subjectName))
-        group='Obese';
-        obese_means = [obese_means averages(i)];
-    else
-        group='Lean';
-        lean_means = [lean_means averages(i)];
+    if xSPM.Ic ==1
+        subjectName = SPM.xY.P{i}(startIndex+1:startIndex+3);
+        if any(strcmp(Obese,subjectName))
+            group='Obese';
+            obese_means = [obese_means averages(i)];
+        else
+            group='Lean';
+            lean_means = [lean_means averages(i)];
+        end
+    elseif xSPM.Ic ==2
+        subjectName = SPM.xY.P{i}(startIndex+1:startIndex+5);
+        if any(strcmp(GLP,subjectName))
+            group='GLP';
+            GLP_means = [GLP_means averages(i)];
+        else
+            group='saline';
+            saline_means = [saline_means averages(i)];
+        end
     end
+
 end
 
-%mean_velocity = [0.2574, 0.1225, 0.1787]; % mean velocity
-%std_velocity = [0.3314, 0.2278, 0.2836];  % standard deviation of velocity
-
-%figure
-%hold on
-%bar(1:3,mean_velocity)
-%errorbar(1:3,mean_velocity,std_velocity,'.')
 
 figure
 hold on
-y = [mean(obese_means); mean(lean_means)];
+if xSPM.Ic == 1
+    y = [mean(obese_means); mean(lean_means)];
+    y_std = [std(obese_means); std(lean_means)];
+else
+    y = [mean(GLP_means); mean(saline_means)];
+    y_std = [std(GLP_means); std(saline_means)];
+end
 x = [1;2];
-y_std = [std(obese_means); std(lean_means)];
 h = bar(x,y);
 errorbar(y,y_std,'.');
 
@@ -47,5 +61,11 @@ errorbar(y,y_std,'.');
 %legend(h,l);
 ax = gca;
 ax.XTick = [1,2];
-ax.XTickLabel = {'Obese','Lean'};
+if xSPM.Ic ==1
+    ax.XTickLabel = {'Obese','Lean'};
+else
+    ax.XTickLabel = {'GLP','Saline'};
+end
 ylabel('Percent Signal Change');
+
+xSPM.roi
--- show_significant_tables.m
+++ show_significant_tables.m
@@ -5,3 +5,7 @@ xSPM = xSPM.xSPM;
 
 [hReg,xSPM,SPM] = spm_results_ui('Setup',xSPM);
 TabDat = spm_list('List',xSPM,hReg);
+extract_mean_values_ROI(xSPM)
+
+
+cd /home/kangik/2016_CHJ/GLP_1/IMG_DATA

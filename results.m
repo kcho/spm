48b516818441f14b8362cabaaf87bc32759e2e65
kangik <kangik@kangikui-MacBook.local>
2016-11-21 19:30:58 +0900

    change to have input of SPM.mat location

--- results.m
+++ results.m
@@ -1,35 +1,34 @@
-% function xSPM = results(contrast, comparisonNum, ROI)
-function [hreg, SPM, xSPM] = results(contrast, comparisonNum, ROI)
-
-% contrast must be a 'str' eg contrast='01'
+function [hReg, SPM, xSPM] = results(matFile, comparisonNum, ROI)
+% matFile : SPM.mat from second level analysis
 % comparisonNum
 % 1 = group effect
 % 2 = medication effect
 % 3 = interaction
 
-secondLevelDir = '/home/kangik/2016_CHJ/GLP_1/IMG_DATA/secondLevel';
+clear 'ROI'
 
 contNameList = {'high_calorie > low_calorie' 'high calorie > neutral' 'low calorie > neutral' 'high_low_food > neutral' 'low calorie > high calorie' 'neutral > high calorie' 'neutral > low calorie' 'high_calorie > baseline' 'low calorie > baseline' 'baseline > high calorie' 'baseline > low calorie'};
-
 
 comparisonName = {'GE_', 'ME_', 'Int_'};
 
 if ~exist('ROI')
-    ROI='/home/kangik/Downloads/spm12/tpm/mask_ICV.nii'
+    [spmLoc, scriptName, ext] = fileparts(which('spm'));
+    ROI=sprintf('%s/tpm/mask_ICV.nii', spmLoc);
 end
+
+[matFileDir, matFileName, matFileExt] = fileparts(which('matFile'));
+[secondLevelDir, contrast] = fileparts(matFileDir);
 
 clear xSPM 
 clear SPM
-%spmMat = strcat('/home/kangik/2016_CHJ/GLP_1/IMG_DATA/secondLevel/',contList(contNum),'/SPM.mat')
-matFileName = sprintf('/home/kangik/2016_CHJ/GLP_1/IMG_DATA/secondLevel/%s/SPM.mat', contrast);
-load(matFileName)
-load '/home/kangik/2016_CHJ/GLP_1/IMG_DATA/secondLevel/hReg.mat'
-%spm_list('List', xSPM, hReg)
-%spm_list('List',SPM,hReg)
-% xSPM      - structure containing SPM, distribution & filtering details
+load(matFile)
+
 xSPM = SPM;
-xSPM.swd      = sprintf('/home/kangik/2016_CHJ/GLP_1/IMG_DATA/secondLevel/%s', contrast);
-xSPM.title    = strcat(comparisonName(comparisonNum), contNameList(str2num(contrast)));
+try
+    xSPM.title    = strcat(comparisonName(comparisonNum), contNameList(str2num(contrast)));
+catch
+    xSPM.title    = sprintf('%s_%s', contrast, comparisonNum); 
+    
 xSPM.Ic       = comparisonNum;
 xSPM.n        = 1;
 xSPM.Z        = [];
@@ -51,7 +50,4 @@ xSPM.thresDesc = 'FWE';
 xSPM.roi      = ROI;
 xSPM.units = {'mm'  'mm'  'mm'};
 
-
-%[SPM,xSPM] = spm_getSPM(xSPM);
-[hreg,xSPM,SPM] = spm_results_ui('Setup',xSPM);
-TabDat = spm_list('List',xSPM,hReg);
+[hReg,xSPM,SPM] = spm_results_ui('Setup',xSPM);

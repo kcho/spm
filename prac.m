acd26a74d698976b23e0bec1d97643d2d49a75c7
kcho <sky8671@gmail.com>
2017-04-07 16:10:07 +0900

    remove

--- prac.m
+++
@@ -1,8 +0,0 @@
-%-----------------------------------------------------------------------
-% Job saved on 05-Dec-2016 20:26:59 by cfg_util (rev $Rev: 6460 $)
-% spm SPM - SPM12 (6685)
-% cfg_basicio BasicIO - Unknown
-%-----------------------------------------------------------------------
-matlabbatch{1}.spm.stats.fmri_est.spmmat = {'/home/kangik/2016_CHJ/GLP_1/IMG_DATA/Obese_vs_Lean_G/01/SPM.mat'};
-matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
-matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
